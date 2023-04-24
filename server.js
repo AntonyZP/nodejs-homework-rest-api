33520816113dc4e900f6d4812dffd3ecf1603c6e
AntonyZP <106816172+AntonyZP@users.noreply.github.com>
2023-04-24 14:08:09 +0300

    add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+const DB_HOST =
+  "mongodb+srv://AntonyZP:90aNy5khJQsfG7Ff@cluster0.qoyx6kc.mongodb.net/db-contacts?retryWrites=true&w=majority";
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Server running. Use our API on port: 3000");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// mongoose.set("strictQuerry", true);
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
