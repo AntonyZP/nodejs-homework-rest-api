b9275b1cada01d6953dbd70ceea21755210e749f
AntonyZP <106816172+AntonyZP@users.noreply.github.com>
2023-04-25 13:53:22 +0300

    add render

---
+++ config.js
@@ -0,0 +1,6 @@
+const DB_HOST =
+  "mongodb+srv://AntonyZP:90aNy5khJQsfG7Ff@cluster0.qoyx6kc.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+module.exports = {
+  DB_HOST,
+};
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const app = require("./app");
 const mongoose = require("mongoose");
-const { DB_HOST } = require("./config");
+const app = require("./app");
 
-console.log(process.env);
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose.set("strictQuery", true);
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
-    console.log("Server running. Use our API on port: 3000");
+    app.listen(PORT, () => {
+      console.log("Database connection successful. Server running.");
+    });
   })
   .catch((error) => {
     console.log(error.message);
