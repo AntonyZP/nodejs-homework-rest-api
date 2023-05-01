82b638747c41f68c2b2c5df9eb424b2e914727b4
AntonyZP <106816172+AntonyZP@users.noreply.github.com>
2023-05-01 22:18:44 +0300

    Update contact.js

--- models/contact.js
+++ models/contact.js
@@ -1,6 +1,6 @@
+const Joi = require("joi");
 const { Schema, model } = require("mongoose");
 const { handleMongooseError } = require("../helpers");
-const Joi = require("joi");
 
 const contactSchema = new Schema(
   {
