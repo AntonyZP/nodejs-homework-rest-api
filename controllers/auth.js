1dad4f7882022d578eb39479cb37ff0a4ad89f34
AntonyZP <106816172+AntonyZP@users.noreply.github.com>
2023-05-15 18:31:43 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -99,6 +99,7 @@ const updateAvatar = async (req, res) => {
   const resultUpload = path.join(avatarsDir, filename);
   const avatarURL = path.join('avatars', filename);
   (await Jimp.read(tempUpload)).resize(250, 250).write(resultUpload);
+  fs.unlink(tempUpload);
   await User.findByIdAndUpdate(_id, { avatarURL });
     res.json({
     avatarURL,
